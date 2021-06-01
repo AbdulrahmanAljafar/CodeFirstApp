23f2fa9aa53be0809d2011b8ed78c435893c723e
Abdulrahman_Aljafar <abdulrahmana.jafar@gmail.com>
2021-06-01 14:10:31 +0300

    Connnect to database

---
+++ CodeFirstApp/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeFirstApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ProductModel> Products { get; set; }
+        public DbSet<CustomerModel> Customers { get; set; }
+    }
+}
---
+++ CodeFirstApp/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstApp.Models
+{
+    public class ProductModel
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Color { get; set; }
+
+    }
+}
